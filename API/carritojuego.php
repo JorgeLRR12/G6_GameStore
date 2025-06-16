8493373b02270ec7dc4610ad992c0fa70c109613
Luis-Anto <luisruizsolano2001@gmail.com>
2025-06-16 15:35:44 -0600

    Comentario

--- API/carritojuego.php
+++ API/carritojuego.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
 
-// Manejar solicitud preflight (CORS)
+// Responder a solicitudes de verificación CORS del navegador
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
