36c3fb5fe081ed3737aeb9f9b024be16ce28c7a9
ucrprogrammerFreddy <freddy.sanchezzamora@ucr.ac.cr>
2025-06-27 08:49:00 -0600

    updateconexion

--- Backend/misc/Conexion.php
+++ Backend/misc/Conexion.php
@@ -3,9 +3,15 @@ class Conexion {
     public static function conectar() {
         // Parámetros de conexión
         $host = 'srv863.hstgr.io';
-        $dbname = 'u484426513_ms125';
-        $usuario = 'u484426513_ms125';
-        $clave = 'N]18zvue';
+        $dbname = 'u484426513_u484426513_ms3';
+        $usuario = 'u484426513_u484426513_ms3';
+        $clave = 'F+gis~i>wC1';
+
+
+            // $host = 'srv863.hstgr.io';
+        // $dbname = 'u484426513_ms125';
+        // $usuario = 'u484426513_ms125';
+        // $clave = 'N]18zvue';
 
         try {
             $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $usuario, $clave);
